053045d36d80d80d33e0b7c0eecbd73895d9aea6
Phillip Silvia <wizcom86@gmail.com>
2024-07-10 16:21:58 -0700

    Migrations completed for building the reservations table and seeding it with initial data

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,10 +5,12 @@ const service = require("./reservations.service");
  * List handler for reservation resources
  */
 async function list(req, res) {
+  console.log("We got to the controller.");
   const data = await service.list();
+  console.log(data);
   res.json({ data: data });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
