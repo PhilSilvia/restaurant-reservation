6c065ddb8919c80797c1eca477c06447e6e937cc
Phillip Silvia <wizcom86@gmail.com>
2024-07-15 12:56:35 -0700

    Added margins to the reservations in reservations table on the dashboard. Added the display for the tables.

--- front-end/src/reservations/ReservationDetails.js
+++ front-end/src/reservations/ReservationDetails.js
@@ -2,23 +2,23 @@ import React from "react";
 
 function ReservationDetails({ first_name, last_name, mobile_number, reservation_date, reservation_time, people }){
     return (
-        <section className="border p-4 mb-4">
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">
-                        {first_name} {last_name}
-                    </h5>
-                    <h6 className="card-subtitle">
-                        {mobile_number}
-                    </h6>
-                    <p className="card-text">
-                        Date: {reservation_date}<br />
-                        Time: {reservation_time}<br />
-                        Party Size: {people}<br />
-                    </p>
-                </div>
+        <div className="card my-2">
+            <div className="card-header">
+                <h5 className="card-title">
+                    {first_name} {last_name}
+                </h5>
+                <h6 className="card-subtitle">
+                    {mobile_number}
+                </h6>
             </div>
-        </section>
+            <div className="card-body">
+                <p className="card-text">
+                    Date: {reservation_date}<br />
+                    Time: {reservation_time}<br />
+                    Party Size: {people}<br />
+                </p>
+            </div>
+        </div>      
     );
 }
 
--- front-end/src/tables/TableDisplay.js
+++ front-end/src/tables/TableDisplay.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function TableDisplay({ table_name, status }){
     return (
-        <div></div>
+        <div className="card my-2">
+            <div className="card-body">
+                <h5 className="card-title">
+                    {table_name}
+                </h5>
+                <p className="card-text">
+                    Status: {status}<br />
+                </p>
+            </div>
+        </div> 
     );
 }
 
