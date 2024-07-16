f2394f6d881f58a096daf70bd92471c23342ab65
Phillip Silvia <wizcom86@gmail.com>
2024-07-16 12:35:32 -0700

    Added a Finish button to the TableDisplays for occupied tables

--- front-end/src/tables/TableDisplay.js
+++ front-end/src/tables/TableDisplay.js
@@ -3,13 +3,20 @@ import React from "react";
 function TableDisplay({ table_name, status, table_id }){
     return (
         <div className="card my-2">
-            <div className="card-body">
+            <div className="card-header">
                 <h5 className="card-title">
                     {table_name}
                 </h5>
+            </div>
+            <div className="card-body">
                 <p className="card-text" data-table-id-status={table_id}>
                     Status: {status}<br />
                 </p>
+                {status === "Occupied" && (
+                    <button className="btn btn-success" data-table-id-finish={table_id}>
+                        Finish
+                    </button>
+                )}
             </div>
         </div> 
     );
