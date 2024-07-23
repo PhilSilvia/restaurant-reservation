53e953d374abb1a3610d06a2d2b509d11fd15675
Phillip Silvia <wizcom86@gmail.com>
2024-07-23 12:45:13 -0700

    Search page and search form added. "Find" button is currently non-functional.

--- front-end/src/search/Search.js
+++ front-end/src/search/Search.js
@@ -1,9 +1,19 @@
 import React from "react";
+import SearchByMobileNumberForm from "./SearchByMobileNumberForm";
 
 function Search(){
     return (
-        <></>
+        <main>
+            <h1>Search for a Reservation</h1>
+            <div className="d-md-flex mb-3">
+                <SearchByMobileNumberForm />
+            </div>
+        </main>
     );
 }
 
 export default Search;
+
+/*
+
+            */
---
+++ front-end/src/search/SearchByMobileNumberForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ErrorAlert from "../layout/ErrorAlert";
+
+function SearchByMobileNumberForm(){
+    const [ mobileNumber, setMobileNumber ] = useState("");
+    const [ submissionError, setSubmissionError ] = useState(null);
+
+    function handleSumbission(event){
+        event.preventDefault();
+    }
+
+    // Event handler for when the form's values are changed, 
+    // so we can track the change in our state variable
+    const handleChange = ({ target }) => {
+        const value = target.value;
+        setMobileNumber(value);
+    };
+
+    return (
+        <div className="col">
+            <form onSubmit={handleSumbission}>
+                <div className="form-group">
+                    <label htmlFor="mobile_number" className="mx-2">
+                        Mobile Number
+                        <input 
+                            type="text"
+                            className="form-control" 
+                            name="mobile_number" 
+                            id="mobile_number" 
+                            onChange={handleChange}
+                            placeholder="Enter a customer's phone number" 
+                            value={mobileNumber}
+                        />
+                    </label>
+                    <button type="submit" className="btn btn-primary">Find</button>
+                </div>
+            </form>
+            <ErrorAlert error={submissionError} />
+        </div>
+    );
+}
+
+export default SearchByMobileNumberForm;
