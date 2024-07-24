df193ac5e0fa7cfd11617d5fd9eda654ac6d2efc
Phillip Silvia <wizcom86@gmail.com>
2024-07-24 12:36:36 -0700

    Added more comments to back-end services.

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,11 +1,20 @@
 const knex = require("../db/connection");
 
+/**
+ * List function for the reservations
+ * @returns A list of all reservations that are not finished
+ */
 async function list(){
     return knex("reservations")
         .select("*")
         .whereNot({ "status": "finished"});
 }
 
+/**
+ * List function for a specific reservation date
+ * @param {string} date The date of the reservations to be listed
+ * @returns A list of all reservations that are not finished that match the given date
+ */
 async function readDate(date){
     return knex("reservations")
         .select("*")
@@ -14,6 +23,11 @@ async function readDate(date){
         .orderBy('reservation_time');
 }
 
+/**
+ * Search function for a particular mobile number
+ * @param {string} mobile_number The mobile number we are searching for
+ * @returns All reservations that match the given mobile number
+ */
 async function searchMobileNumber(mobile_number){
     return knex("reservations")
         .whereRaw(
@@ -23,12 +37,22 @@ async function searchMobileNumber(mobile_number){
         .orderBy("reservation_date");
 }
 
+/**
+ * Read function that returns the single reservation matching the given id
+ * @param {number} reservationId The id of the reservation
+ * @returns The reservation matching the given id
+ */
 async function read(reservationId){
     return knex("reservations")
         .select("*")
         .where({"reservation_id": reservationId});
 }
 
+/**
+ * Create function that adds a new reservation with the given values to the database
+ * @param {object} reservation The data for the new reservation
+ * @returns The created reservation
+ */
 async function create(reservation){
     return knex("reservations")
         .insert(reservation)
@@ -36,6 +60,11 @@ async function create(reservation){
         .then((createdRecords) => createdRecords[0]);
 }
 
+/**
+ * Update function that changes the values of a given reservation
+ * @param {object} updatedReservation The new data to be updated
+ * @returns The updated reservation
+ */
 async function update(updatedReservation){
     return knex("reservations")
         .select("*")
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,16 +1,30 @@
 const knex = require("../db/connection");
 
+/**
+ * List function that returns all tables from the database
+ * @returns All tables from the database
+ */
 async function list(){
     return knex("tables").select("*")
         .orderBy("table_name");
 }
 
+/**
+ * Read function that returns the data for the table matching a given id from the databse
+ * @param {number} tableId The id of the desired table
+ * @returns The information for the given table id
+ */
 async function read(tableId){
     return knex("tables")
         .select("*")
         .where({"table_id": tableId});
 }
 
+/**
+ * Create function to add a new table to the database
+ * @param {object} newTable The values for the new table
+ * @returns The newly created table
+ */
 async function create(newTable){
     return knex("tables")
         .insert(newTable)
@@ -18,6 +32,11 @@ async function create(newTable){
         .then((createdRecords) => createdRecords[0]);
 }
 
+/**
+ * Update function to updates the values for a table
+ * @param {object} updatedTable Values to be updated
+ * @returns The newly updated table
+ */
 async function update(updatedTable){
     return knex("tables")
         .select("*")
@@ -26,6 +45,11 @@ async function update(updatedTable){
         .then((updated) => updated[0]);
 }
 
+/**
+ * Update function that will attempt to update a reservation and a table, doing neither if either fails.
+ * @param {object} updatedTable The table values to be updated
+ * @param {object} updatedReservation The reservation values to be updated
+ */
 async function updateTableAndReservation(updatedTable, updatedReservation){
     try {
         await knex.transaction(async (trx) => {
